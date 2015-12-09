b118ac2e6913c8f620b13f7da9c0d4abf5821922
malex <mircea.alexandru@gmail.com>
2015-12-09 23:27:57 +0200

    linting

--- test/postgres.test.js
+++ test/postgres.test.js
@@ -1,11 +1,8 @@
-/*jslint node: true*/
-/*jslint asi: true */
-/*global describe:true, it:true*/
-"use strict";
+'use strict'
 
 var assert = require('assert')
 var seneca = require('seneca')
-var Lab = require('lab');
+var Lab = require('lab')
 var lab = exports.lab = Lab.script()
 
 var describe = lab.describe
@@ -13,8 +10,7 @@ var it = lab.it
 
 var shared = require('seneca-store-test')
 
-
-var si = seneca();
+var si = seneca()
 si.use(require('..'), {
   name: 'senecatest',
   host: '127.0.0.1',
@@ -24,34 +20,36 @@ si.use(require('..'), {
   options: { }
 })
 
-si.__testcount = 0
-var testcount = 0
-
-
-describe('postgres', function () {
-  it('basic', function (done) {
-    testcount++
-    shared.basictest(si, done)
+describe('Basic Test', function () {
+  shared.basictest({
+    seneca: si,
+    script: lab
   })
 
-  it('save with passing an id$', function(done) {
+  shared.sorttest({
+    seneca: si,
+    script: lab
+  })
 
+  shared.limitstest({
+    seneca: si,
+    script: lab
+  })
+})
+
+describe('postgres', function () {
+  it('save with passing an id$', function (done) {
     var product = si.make('foo')
 
     product.id$ = '12345'
     product.p1 = 'pear'
 
-    si.act(
-      { role:'entity', cmd:'save', ent: product},
-      function( err, product ) {
+    si.act({role: 'entity', cmd: 'save', ent: product},
+      function (err, product) {
         console.log(arguments)
         assert(!err)
         assert.equal(product.id, '12345')
         done()
       })
-  })
-
-  it('close', function (done) {
-    shared.closetest(si, testcount, done)
   })
 })
