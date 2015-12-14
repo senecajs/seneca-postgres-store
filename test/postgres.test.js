1909faa847bc1585807c89ab8c5789ba483ec1c3
malex <mircea.alexandru@gmail.com>
2015-12-14 13:56:08 +0200

    added implementation for ne$

--- test/postgres.test.js
+++ test/postgres.test.js
@@ -4,8 +4,10 @@ var assert = require('assert')
 var seneca = require('seneca')
 var Lab = require('lab')
 var lab = exports.lab = Lab.script()
+var Async = require('async')
 
 var describe = lab.describe
+var before = lab.before
 var it = lab.it
 
 var shared = require('seneca-store-test')
@@ -44,11 +46,52 @@ describe('postgres', function () {
     product.p1 = 'pear'
 
     si.act({role: 'entity', cmd: 'save', ent: product},
-      function (err, product) {
-        assert(!err)
-        assert.equal(product.id, '12345')
-        done()
-      })
+        function (err, product) {
+          assert(!err)
+          assert.equal(product.id, '12345')
+          done()
+        })
+  })
+})
+
+describe('postgres store API V2.0.0', function () {
+  before(function (done) {
+    var Product = si.make('product')
+
+    Async.series([
+      function clear (next) {
+        Product.remove$({ all$: true }, next)
+      },
+      function create (next) {
+        var products = [
+          Product.make$({ name: 'apple', price: 100 }),
+          Product.make$({ name: 'pear', price: 200 }),
+          Product.make$({ name: 'cherry', price: 300 })
+        ]
+
+        function saveproduct (product, saved) {
+          product.save$(saved)
+        }
+
+        Async.forEach(products, saveproduct, next)
+      }
+    ], function (err) {
+      assert(!err)
+      done()
+    })
+  })
+
+  it('use not equal', function (done) {
+    var product = si.make('product')
+
+    product.list$({ price: {ne$: 200}, sort$: {price: 1} }, function (err, lst) {
+      assert(!err)
+
+      assert.equal(2, lst.length)
+      assert.equal('apple', lst[0].name)
+      assert.equal('cherry', lst[1].name)
+      done()
+    })
   })
 })
 
