38d412645860d93313d01743faf2407982e1a501
lilsweetcaligula <15699226+lilsweetcaligula@users.noreply.github.com>
2021-06-30 06:51:57 +0300

    Tidy up

--- postgresql-store.js
+++ postgresql-store.js
@@ -3,173 +3,66 @@
 const _ = require('lodash')
 const Assert = require('assert')
 const Pg = require('pg')
-const Uuid = require('uuid')
-const RelationalStore = require('./lib/relational-util')
 
-const Util = require('util')
 const { intern } = require('./lib/intern')
 const { asyncMethod } = intern
 
 const STORE_NAME = 'postgresql-store'
 const ACTION_ROLE = 'sql'
 
-const MIN_WAIT = 16
-const MAX_WAIT = 5000
-
-module.exports = function (opts) {
+module.exports = function (options) {
   const seneca = this
 
-  opts.minwait = opts.minwait || MIN_WAIT
-  opts.maxwait = opts.maxwait || MAX_WAIT
-
   const ColumnNameParsing = {
-    fromColumnName: opts.fromColumnName || _.identity,
-    toColumnName: opts.toColumnName || _.identity
+    fromColumnName: options.fromColumnName || _.identity,
+    toColumnName: options.toColumnName || _.identity
   }
   const QueryBuilder = require('./lib/query-builder')(ColumnNameParsing)
   const Q = require('./lib/qbuilder')
 
 
-  let minwait
-  const internals = {}
-
-  function error (query, args, err/*, next*/) {
-    if (err) {
-      const errorDetails = {
-        message: err.message,
-        err: err,
-        stack: err.stack,
-        query: query
-      }
-      seneca.log.error('Query Failed', JSON.stringify(errorDetails, null, 1))
-      // next ({code: 'entity/error', store: STORE_NAME})
-
-      if ('ECONNREFUSED' === err.code || 'notConnected' === err.message || 'Error: no open connections' === err) {
-        minwait = opts.minwait
-        if (minwait) {
-          reconnect(args)
-        }
-      }
-
-      return true
-    }
-
-    return false
-  }
-
-  function reconnect (args) {
-    seneca.log.debug('attempting db reconnect')
-
-    configure(opts, function (err) {
-      if (err) {
-        seneca.log.debug('db reconnect (wait ' + opts.minwait + 'ms) failed: ' + err)
-        minwait = Math.min(2 * minwait, opts.maxwait)
-        setTimeout(function () {
-          reconnect(args)
-        }, minwait)
-      }
-      else {
-        minwait = opts.minwait
-        seneca.log.debug('reconnect ok')
-      }
-    })
-  }
-
-  let pgConf
   let dbPool
 
-  function configure (spec, done) {
-    pgConf = 'string' === typeof (spec) ? null : spec
+  function configure(spec, done) {
+    const conf = intern.getConfig(spec)
 
-    if (!pgConf) {
-      pgConf = {}
-
-      const urlM = /^postgres:\/\/((.*?):(.*?)@)?(.*?)(:?(\d+))?\/(.*?)$/.exec(spec)
-      pgConf.name = urlM[7]
-      pgConf.port = urlM[6]
-      pgConf.host = urlM[4]
-      pgConf.username = urlM[2]
-      pgConf.password = urlM[3]
-
-      pgConf.port = pgConf.port ? parseInt(pgConf.port, 10) : null
-    }
-
-    // pg conf properties
-    pgConf.user = pgConf.username
-    pgConf.database = pgConf.name
-
-    pgConf.host = pgConf.host || pgConf.server
-    pgConf.username = pgConf.username || pgConf.user
-    pgConf.password = pgConf.password || pgConf.pass
-
-    // TODO: CLEAN UP. spec MUST MAKE IT THROUGH TO THE PG DRIVER !!!
-    // The reason it is like so now, is because the driver breaks if
-    // you try to pass any Object value.
-    //
     dbPool = new Pg.Pool({
-      name: pgConf.name,
-      host: pgConf.host,
-      port: pgConf.port,
-      username: pgConf.username,
-      password: pgConf.password,
-      minwait: pgConf.minwait,
-      maxwait: pgConf.maxwait,
-      user: pgConf.user,
-      database: pgConf.database
+      user: conf.user,
+      host: conf.host,
+      database: conf.database,
+      password: conf.password,
+      port: conf.port
     })
 
     return done()
   }
 
-  async function withDbClient(dbPool, f) {
-    const client = await dbPool.connect()
-
-    let result
-
-    try {
-      result = await f(client)
-    } finally {
-      client.release()
-    }
-
-    return result
-  }
-
-  async function execQuery(query, ctx) {
-    if (!query) {
-      throw new Error('An empty query is not a valid query')
-    }
-
-    const { client } = ctx
-
-    return client.query(query)
-  }
-
   const store = {
     name: STORE_NAME,
 
-    close: function (args, done) {
+    close: function (_msg, done) {
       dbPool.end().then(done).catch(done)
     },
 
     save: asyncMethod(async function (msg) {
       const seneca = this
 
-      return withDbClient(dbPool, async (client) => {
+      return intern.withDbClient(dbPool, async (client) => {
         const ctx = { seneca, client }
 
         const { ent, q } = msg
         const { auto_increment$: autoIncrement = false } = q
 
-        if (isUpdate(ent)) {
-          return updateEnt(ent, ctx)
+        if (intern.isUpdate(msg)) {
+          return intern.updateEnt(ent, ctx)
         }
 
 
         const newEnt = ent.clone$()
 
         if (!autoIncrement) {
-          const generatedId = await generateId(seneca)
+          const generatedId = await intern
+            .askSenecaToGenerateId({ role: ACTION_ROLE, target: STORE_NAME }, ctx)
 
           const newId = null == ent.id$
             ? generatedId
@@ -179,66 +72,43 @@ module.exports = function (opts) {
         }
 
 
-        const upsertFields = maybeUpsert(ent, q)
+        const upsertFields = intern.maybeUpsert(msg)
 
         if (null != upsertFields) {
-          return upsertEnt(upsertFields, newEnt, q, ctx)
+          return intern.upsertEnt(upsertFields, newEnt, q, ctx)
         }
 
-        return insertEnt(newEnt, ctx)
-
-
-        function maybeUpsert(ent, q) {
-          if (isUpdate(ent)) {
-            return null
-          }
-
-          if (!Array.isArray(q.upsert$)) {
-            return null
-          }
-
-          const upsertFields = q.upsert$.filter((p) => !p.includes('$'))
-
-          if (0 === upsertFields.length) {
-            return null
-          }
-
-          return upsertFields
-        }
-
-        function isUpdate(ent) {
-          return null != ent.id
-        }
+        return intern.insertEnt(newEnt, ctx)
       })
     }),
 
     load: asyncMethod(async function (msg) {
       const seneca = this
 
-      return withDbClient(dbPool, async (client) => {
+      return intern.withDbClient(dbPool, async (client) => {
         const ctx = { seneca, client }
         const { qent, q } = msg
 
-        return findEnt(qent, q, ctx)
+        return intern.findEnt(qent, q, ctx)
       })
     }),
 
     list: asyncMethod(async function (msg) {
       const seneca = this
 
-      return withDbClient(dbPool, async (client) => {
+      return intern.withDbClient(dbPool, async (client) => {
         const ctx = { seneca, client }
         const { qent, q } = msg
 
         const nativeQuery = isNativeQuery(q)
 
         if (null == nativeQuery) {
-          return listEnts(qent, q, ctx)
+          return intern.listEnts(qent, q, ctx)
         }
 
-        const { rows } = await execQuery(nativeQuery, ctx)
+        const { rows } = await intern.execQuery(nativeQuery, ctx)
 
-        return rows.map(row => makeEntOfRow(row, qent))
+        return rows.map(row => intern.makeEntOfRow(row, qent))
       })
 
       function isNativeQuery(q) {
@@ -265,273 +135,35 @@ module.exports = function (opts) {
     remove: asyncMethod(async function (msg) {
       const seneca = this
 
-      return withDbClient(dbPool, async (client) => {
+      return intern.withDbClient(dbPool, async (client) => {
         const ctx = { seneca, client }
         const { qent, q } = msg
 
-        return removeEnt(qent, q, ctx)
+        return intern.removeEnt(qent, q, ctx)
       })
     }),
 
-    native: function (msg, done) {
+    native: function (_msg, done) {
       dbPool.connect().then(done).catch(done)
     }
   }
 
-  internals.transformDBRowToJSObject = function (row) {
-    const obj = {}
 
-    for (const attr in row) {
-      if (row.hasOwnProperty(attr)) {
-        obj[ColumnNameParsing.fromColumnName(attr)] = row[attr]
-      }
-    }
-
-    return obj
-  }
+  const meta = seneca.store.init(seneca, options, store)
 
 
-  const meta = seneca.store.init(seneca, opts, store)
-
-  seneca.add({ init: store.name, tag: meta.tag }, function (args, done) {
-    return configure(opts, done)
+  seneca.add({ init: store.name, tag: meta.tag }, function (_msg, done) {
+    return configure(options, done)
   })
 
-  seneca.add({ role: ACTION_ROLE, hook: 'generate_id', target: STORE_NAME }, function (args, done) {
-    return done(null, { id: Uuid() })
-  })
 
-  return {name: store.name, tag: meta.tag}
-
-
-  async function generateId(seneca) {
-    const act = Util.promisify(seneca.act).bind(seneca)
-    const result = await act({ role: ACTION_ROLE, hook: 'generate_id', target: STORE_NAME })
-
-    const { id: newId } = result
-
-    return newId
-  }
-
-  async function insertEnt(ent, ctx) {
-    const { client } = ctx
-    const escapeIdentifier = client.escapeIdentifier.bind(client)
-
-    const ent_table = RelationalStore.tablename(ent)
-    const entp = RelationalStore.makeentp(ent)
-
-    const ins_query = Q.insertstm({
-      into: ent_table,
-      values: intern.compact(entp),
-      escapeIdentifier
+  seneca.add(intern.msgForGenerateId({ role: ACTION_ROLE, target: STORE_NAME }),
+    function (_msg, done) {
+      const id = intern.generateId()
+      return done(null, { id })
     })
 
-    const insert = await execQuery(ins_query, ctx)
 
-    return makeEntOfRow(insert.rows[0], ent)
-  }
-
-  async function findEnt(ent, q, ctx) {
-    const { client } = ctx
-    const ent_table = RelationalStore.tablename(ent)
-
-    const query = Q.selectstm({
-      columns: '*',
-      from: ent_table,
-      where: whereOfQ(q, ctx),
-      limit: 1,
-      offset: 0 <= q.skip$ ? q.skip$ : null,
-      order_by: q.sort$ || null,
-      escapeIdentifier: client.escapeIdentifier.bind(client)
-    })
-
-    const { rows } = await execQuery(query, ctx)
-
-    if (rows.length > 0) {
-      return makeEntOfRow(rows[0], ent)
-    }
-
-    return null
-  }
-
-  async function listEnts(ent, q, ctx) {
-    const { client } = ctx
-    const ent_table = RelationalStore.tablename(ent)
-
-    const query = Q.selectstm({
-      columns: '*',
-      from: ent_table,
-      where: whereOfQ(q, ctx),
-      limit: 0 <= q.limit$ ? q.limit$ : null,
-      offset: 0 <= q.skip$ ? q.skip$ : null,
-      order_by: q.sort$ || null,
-      escapeIdentifier: client.escapeIdentifier.bind(client)
-    })
-
-    const { rows } = await execQuery(query, ctx)
-
-    return rows.map((row) => makeEntOfRow(row, ent))
-  }
-
-  function whereOfQ(q, ctx) {
-    if ('string' === typeof q || Array.isArray(q)) {
-      return { id: q }
-    }
-
-    const { seneca } = ctx
-
-    return seneca.util.clean(q)
-  }
-
-  async function upsertEnt(upsert_fields, ent, q, ctx) {
-    const { client } = ctx
-    const escapeIdentifier = client.escapeIdentifier.bind(client)
-
-    const ent_table = RelationalStore.tablename(ent)
-    const entp = RelationalStore.makeentp(ent)
-
-    const insert_values = intern.compact(entp)
-    const set_values = intern.compact(entp); delete set_values.id
-
-    const query = Q.insertstm({
-      into: ent_table,
-      values: insert_values,
-      on_conflict: {
-        columns: upsert_fields,
-        do_update: {
-          set: set_values
-        }
-      },
-      escapeIdentifier
-    })
-
-    const { rows } = await execQuery(query, ctx)
-
-    return makeEntOfRow(rows[0], ent)
-  }
-
-  async function updateEnt(ent, ctx) {
-    const { client } = ctx
-    const escapeIdentifier = client.escapeIdentifier.bind(client)
-
-    const ent_table = RelationalStore.tablename(ent)
-    const entp = RelationalStore.makeentp(ent)
-
-    const { id: ent_id } = ent
-
-    const update_query = Q.updatestm({
-      table: ent_table,
-      set: intern.compact(entp),
-      where: { id: ent_id },
-      escapeIdentifier
-    })
-
-    const update = await execQuery(update_query, ctx)
-    const updated_anything = update.rows.length > 0
-
-    if (updated_anything) {
-      return makeEntOfRow(update.rows[0], ent)
-    }
-
-
-    // TODO: Re-write using upserts on the id column:
-    //
-    const ins_query = Q.insertstm({
-      into: ent_table,
-      values: intern.compact(entp),
-      escapeIdentifier
-    })
-
-    const insert = await execQuery(ins_query, ctx)
-
-    return makeEntOfRow(insert.rows[0], ent)
-  }
-
-  async function removeEnt(ent, q, ctx) {
-    if (q.all$) {
-      return removeManyEnts(ent, q, ctx)
-    }
-
-    return removeOneEnt(ent, q, ctx)
-
-
-    async function removeOneEnt(ent, q, ctx) {
-      const { seneca, client } = ctx
-
-      const ent_table = RelationalStore.tablename(ent)
-      const escapeIdentifier = client.escapeIdentifier.bind(client)
-
-
-      const sel_query = Q.selectstm({
-        columns: ['id'],
-        from: ent_table,
-        where: seneca.util.clean(q),
-        limit: 1,
-        offset: 0 <= q.skip$ ? q.skip$ : null,
-        order_by: q.sort$ || null,
-        escapeIdentifier
-      })
-
-      const { rows: sel_rows } = await execQuery(sel_query, ctx)
-
-
-      const del_query = Q.deletestm({
-        from: ent_table,
-        where: {
-          id: sel_rows.map(x => x.id)
-        },
-        escapeIdentifier
-      })
-
-      const { rows: del_rows } = await execQuery(del_query, ctx)
-
-      if (q.load$) {
-        return 0 < del_rows.length
-          ? makeEntOfRow(del_rows[0], ent)
-          : null
-      }
-
-      return null
-    }
-
-
-    async function removeManyEnts(ent, q, ctx) {
-      const { seneca, client } = ctx
-
-      const ent_table = RelationalStore.tablename(ent)
-      const escapeIdentifier = client.escapeIdentifier.bind(client)
-
-
-      const sel_query = Q.selectstm({
-        columns: ['id'],
-        from: ent_table,
-        where: seneca.util.clean(q),
-        limit: 0 <= q.limit$ ? q.limit$ : null,
-        offset: 0 <= q.skip$ ? q.skip$ : null,
-        order_by: q.sort$ || null,
-        escapeIdentifier
-      })
-
-      const { rows } = await execQuery(sel_query, ctx)
-
-
-      const del_query = Q.deletestm({
-        from: ent_table,
-        where: {
-          id: rows.map(x => x.id)
-        },
-        escapeIdentifier
-      })
-
-      await execQuery(del_query, ctx)
-
-      return
-    }
-  }
-
-  function makeEntOfRow(row, baseEnt) {
-    const attrs = internals.transformDBRowToJSObject(row)
-    return RelationalStore.makeent(baseEnt, attrs)
-  }
+  return { name: store.name, tag: meta.tag }
 }
 
